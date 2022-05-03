2f52857b10adf0b61edd058d8af4c810fec4dea8
XF <1441635369@qq.com>
2022-05-03 18:47:54 +0800

    second commit

--- src/main/java/com/dxc/git/GITtest.java
+++ src/main/java/com/dxc/git/GITtest.java
@@ -3,5 +3,6 @@ package com.dxc.git;
 public class GITtest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
